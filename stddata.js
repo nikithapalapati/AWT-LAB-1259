2bf18d299dd2b12c85820940501040632d4bf019
nikithapalapati <p.y.nikitha@gmail.com>
2022-03-09 14:59:36 +0530

    let,var,const,arrowfunction,module 9/02/2022

---
+++ stddata.js
@@ -0,0 +1,11 @@
+let sname = "Nikitha";
+let sdata ={
+    getName : function(){
+        return sname;
+    },
+    setName : function(snewName)
+    {
+        sname = snewName;
+    }
+}
+export default sdata
